18508cdc46070248b644bc5779f3036cc6ef684c
Nakshathra Shiju <its.nakshathrashiju@gmail.com>
2026-08-09 22:35:26 +0530

    Update profile photo

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import profilePic from '../assets/user.jpeg';
+import profilePic from '../assets/user1.jpeg';
 
 function About() {
   return (
